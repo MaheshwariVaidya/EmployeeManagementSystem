34bf8e538cce599b9b41a09f84380baaa8c94b5b
Maheshwari <mahivaidy@gmail.com>
2026-07-10 17:53:16 +0530

    a and b variable added

--- Demo.java
+++ Demo.java
@@ -1,10 +1,16 @@
 public class Demo{
     public static void main(String[] args){
+        //JIRA Story-101
+        public void m1(){
+            
+        }
         System.out.println("Hello world");
 
         //JIRA STORY - 102
         public void m2(){
             //logic
         }
+        int a=40;
+        int b=50;
     }
 }
