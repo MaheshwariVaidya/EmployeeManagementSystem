4aa034ecf7b91d60d2474839bca4243aa13e1f52
Maheshwari <mahivaidy@gmail.com>
2026-07-10 17:12:21 +0530

    102 story completed

--- Demo.java
+++ Demo.java
@@ -1,5 +1,10 @@
 public class Demo{
     public static void main(String[] args){
         System.out.println("Hello world");
+
+        //JIRA STORY - 102
+        public void m2(){
+            //logic
+        }
     }
 }
