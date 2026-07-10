4399dc35efa5c156944be54dfcdf1ac8735eeba3
Maheshwari <mahivaidy@gmail.com>
2026-07-10 18:01:52 +0530

    constructor added

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
 public class Demo{
+    public Demo(){
+
+    }
     public static void main(String[] args){
         //JIRA Story-101
         public void m1(){
             
         }
-        System.out.println("Hello world");
-
+       
         //JIRA STORY - 102
         public void m2(){
             //logic
